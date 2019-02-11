92223f9a845efcc61bdfd5e3edc75ba0a99b8eb9
David Zukor <zukord@gmail.com>
2019-02-10 19:23:49 -0500

    HTML appears complete, js/jquery and bootstrap remain.

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+var crystal1 = Math.floor(Math.random() * 12 + 1);
+var crystal2 = Math.floor(Math.random() * 12 + 1);
+var crystal3 = Math.floor(Math.random() * 12 + 1);
+var crystal4 = Math.floor(Math.random() * 12 + 1);
+var numberToGuess = Math.floor(Math.random() * 102 + 19);
+
+function playGame(){
+
+}
